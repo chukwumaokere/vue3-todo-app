ba944b944c7fc6b78f0e1af5abc49368d45b248d
chukwumaokere <chukwumaokere@yahoo.com>
2020-10-03 20:03:43 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,29 @@ export default {
 
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: sans-serif;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-size: 2em;
+  width: 80%;
+  margin: 0 auto;
+}
+input, textarea, button, p, div, section, article, select {
+  display: 'block';
+  width: 100%;
+  font-family: sans-serif;
+  font-size: 1em;
+  margin: 0.5em;
+}
+.todo {
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
