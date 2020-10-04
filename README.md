2a05216e067c9507566fc40e3fad5a133d078ee6
chukwumaokere <chukwumaokere@yahoo.com>
2020-10-03 22:23:22 -0500

    update

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 
 # Vue.js Todo App
 
-* [ ] Setup
-* [ ] Create New Todo Form
-* [ ] Call a function when the form is submitted
-* [ ] Create a string property to store the user input
-  * [ ] Watch the property change as we type in the input
-* [ ] Log the user input when the form is submitted
-* [ ] Create an array property for todos
-* [ ] Push the new todo into the todos array, with done: false
-* [ ] Show the todos in a list
-* [ ] Check done on a todo to mark it as done
-  * [ ] Show a line through the todo text
-* [ ] Add a button to delete a todo
-* [ ] Add a button to mark all todos as done
+* [x] Setup
+* [x] Create New Todo Form
+* [x] Call a function when the form is submitted
+* [x] Create a string property to store the user input
+  * [x] Watch the property change as we type in the input
+* [x] Log the user input when the form is submitted
+* [x] Create an array property for todos
+* [x] Push the new todo into the todos array, with done: false
+* [x] Show the todos in a list
+* [x] Check done on a todo to mark it as done
+  * [x] Show a line through the todo text
+* [x] Add a button to delete a todo
+* [x] Add a button to mark all todos as done
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Vue 3 Todo App</h1>
+  <form @submit.prevent="addNewTodo">
+    <label>New Todo</label>
+    <input v-model="newTodo" name="newTodo">
+    <button>Add New Todo</button>
+  </form>
+  <button @click="markAllDone()">Mark All As Done</button>
+  <button @click="deleteAll()">Delete All</button>
+  <button @click="deleteSelected()">Deleted Selected</button>
+  <div>
+    <ol> 
+      <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+        <input type="checkbox" @click="addToSelected(index)">
+        <h3 @click="toggleDone(todo)" :class="{ done: todo.done }">{{todo.content}}</h3>
+        <button @click="removeTodo(index)">Remove Todo</button>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
+  setup(){
+    const newTodo = ref('');
+    const todos = ref([]);
+    const selected = ref([]);
 
+    function addNewTodo(){ 
+      todos.value.push({
+        content: newTodo.value,
+        id: Date.now(),
+        done: false,
+      });
+      newTodo.value = '';
+    }
+    function toggleDone(todo){
+      todo.done = !todo.done;
+    }
+    function removeTodo(index){
+      todos.value.splice(index, 1);
+    }
+    function markAllDone(){
+      todos.value.forEach(todo => { 
+        if(todo.done !== true){
+          todo.done = true;
+        }
+      })
+    }
+    function deleteAll(){
+      todos.value = [];
+    }
+    function addToSelected(index){
+      selected.value.push(index);
+      console.log(selected.value);
+    }
+    function deleteSelected(){
+      selected.value.forEach(index => {
+        if(index > 0){
+          todos.value.splice(index-1, 1);  
+        }else{
+          todos.value.splice(index, 1);
+        }
+      })
+    }
+
+    return {
+      addNewTodo,
+      newTodo,
+      todos,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      deleteAll,
+      addToSelected,
+      deleteSelected
+    }
   }
 }
 
